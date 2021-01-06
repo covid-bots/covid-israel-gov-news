f3667e972f4fbafa78d52fb39bb807b9d84d3986
orik1504 <76853825+orik1504@users.noreply.github.com>
2021-01-06 23:02:56 +0200

    ⚡ Now using the News JSON API instead of web scraping (#4)

--- modules.py
+++ modules.py
@@ -21,131 +21,89 @@ class NewsArticle(BaseModule):
     Example article can be found in https://www.gov.il/he/departments/news/03012021-03
     """
 
-    def __init__(self, url: str = None, data: BeautifulSoup = None):
-        """ Generates an instance, and saves the url in it. Creating an instance
-        doesn't automatically makes a https request (this can be done be calling
-        the `request` method). """
-
-        args_count = sum(cur is not None for cur in [data, url])
-        if args_count != 1:
-            raise ValueError(
-                "One argument exactly must be specified - either `data` or `url`")
-
-        self.__url = url
-
-        # when the `request` methond is called,
-        # it updates this property and saves in it the
-        # `beautiful soup` instance.
+    def __init__(self, data: dict):
         self.__data = data
-
-    def request(self,) -> None:
-        """ Makes a http request to the article URL, analyses the data and generates
-        the object properties. Returns `None`. """
-
-        if self.__data is not None:
-            # make a request only if there is no data.
-            return
-
-        response = requests.get(self.__url)
-        self.__data = BeautifulSoup(response.content, 'lxml')
 
     @property
     def title(self,) -> str:
         """ The title of the article, as a string. """
-        self.request()
-        element = self.__data.find(id='NewsTitle')
-
-        if element is None or not element.text.strip():
-            return None
-
-        return element.text.strip()
+        return self.__data['Title']
 
     @property
     def subtitle(self,) -> str:
         """ The subtitle of the article, as a string. """
-        self.request()
-        element = self.__data.find(id='NewsDescription')
-
-        if element is None or not element.text.strip():
-            return None
-
-        return element.text.strip()
+        return self.__data['Description']
 
     @property
-    def subject(self,) -> str:
+    def subject(self,) -> typing.List[str]:
         """ The subject of the article, as a string. """
-        self.request()
-
-        element = self.__data.find(id='md_content_subject')
-
-        if element is None:
-            return None
-
-        return element.text.strip()
+        return [topic_data['Title'] for topic_data in self.__data['ConnectedTopics']] 
 
     @property
     def subsubjects(self,) -> typing.List[str]:
         """ A list of the 'sub-subjects' of the article. Each element in the list
         is a string. """
-        self.request()
-        container = self.__data.find(id='md_content_subSubject')
-
-        if container is None:
-            return list()
-
-        return [item.text.strip() for item in container.find_all('span')]
-
+        return [topic_data['Title'] for topic_data in self.__data['ConnectedSubTopics']] 
+    
     @property
-    def posted_string(self,) -> str:
+    def publish_date_string(self,) -> str:
         """ The date the article was posted on, as a string (raw, received
         from the request) """
-        self.request()
-        return self.__data.find(id='md_content_publishDate').text.strip()
+        return self.__data['PublishDate']
 
     @property
-    def posted_date(self,) -> datetime.date:
-        """ A `datetime.date` instance that represents the date that the article
-        was posted on. """
-        day, month, year = self.posted_string.split('.')
-        return datetime.date(day=int(day), month=int(month), year=int(year))
+    def update_date_string(self,) -> str:
+        """ The date the article was posted on, as a string (raw, received
+        from the request) """
+        return self.__data['UpdateDate']
 
     @property
-    def content(self,) -> str:
+    def content_html(self,) -> str:
         """ Returns the content of the article, as a string. """
+        return self.__data['Content']
 
     @property
-    def content_lines(self,) -> typing.List[str]:
+    def content_text(self,) -> str:
+        soup = BeautifulSoup(self.content_html, 'lxml')
+        return soup.get_text()
+    
+    @property
+    def content_sentences(self,) -> typing.List[str]:
         """ Returns the content of the article, represented as a list of sentences
         (list of strings). """
 
+        lines = self.content_text.split('.')
+        return [
+            line.strip() 
+            for line in lines 
+            if line.strip() 
+        ]
+
     @property
-    def attached_files(self,) -> typing.List[str]:
-        """ Returns a list of urls to attached files to the article. If
-        there are no files attached, returns an empty list.
-
-        An example of an article with an attached file can be found on:
-        https://www.gov.il/he/departments/news/agra_2021 """
-
-        self.request()
-        container = self.__data.find(id='divFiles')
-
-        if container is None:
-            return list()
-
-        return [item['href'] for item in container.find_all('a', href=True, title=True)]
-
+    def type(self,) -> str:
+        return self.__data['NewsTypeDesc']
 
 class GovIlNews(BaseModule):
 
-    ENDPOINT = r'https://www.gov.il/he/departments/news'
+    def __init__(self, url: str):
+        response = requests.get(url)
+        self._data = response.json()
 
-    def search(self, skip: int = 0, limit: int = 10, **extra_kwargs) -> typing.List[NewsArticle]:
-        """ By default, searches for the most recant 10 articles, and returns
-        them in a list, where the first element in the list is the newest article,
-        and the 9th is the older one. Use `extra_kwargs` to filter the articles
-        by different departments, subjects, and more! """
+    def articles(self) -> typing.List[NewsArticle]:
+        return [NewsArticle(article_data) for article_data in self._data['results']]
+    
+    def latest_article(self) -> NewsArticle:
+        return NewsArticle(self._data['results'][0])
 
-    def article(self, name: str) -> NewsArticle:
-        """ Recives the article name (usually a string that inclues the date
-        of the article and some additional numbers), and returns a `NewsArticle`
-        instance that represents the article. """
+    @property
+    def num_of_articles(self) -> int:
+        return len(self._data['results'])
+
+    
+
+class CovidGovIlNews(GovIlNews):
+    
+    __API_URL = r'https://www.gov.il/he/api/NewsApi/Index?limit=100&OfficeId=104cb0f4-d65a-4692-b590-94af928c19c0&topic=3ef9cac8-a1a9-4352-91d4-860efd3b720d&skip=0'
+
+    def __init__(self):
+        super().__init__(self.__API_URL)
